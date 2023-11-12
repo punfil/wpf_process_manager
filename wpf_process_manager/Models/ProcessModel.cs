554a7dea14aed8c88667e1ddf00ff99dc164b7fd
Wojciech Panfil <wojtek.panfil@hotmail.com>
2023-11-12 11:26:35 +0100

    Add process manager
    Signed-off-by: Wojciech Panfil <wojtek.panfil@hotmail.com>

---
+++ wpf_process_manager/Models/ProcessModel.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace wpf_process_manager.Models
+{
+    public class ProcessModel
+    {
+        private Process _process;
+
+        public ProcessModel(Process process)
+        {
+            this._process = process;
+        }
+
+        public int GetPID()
+        {
+            return this._process.Id;
+        }
+
+        public string GetName()
+        {
+            return this._process.ProcessName;
+        }
+
+        public string GetCPUUsage()
+        {
+            double cpuUsage = (double)_process.TotalProcessorTime.TotalMilliseconds /
+                (double)DateTime.Now.Subtract(_process.StartTime).TotalMilliseconds * 100.0;
+
+            return cpuUsage.ToString();
+        }
+
+        public string GetMemoryUsage()
+        {
+            return _process.PagedMemorySize64.ToString();
+        }
+
+        public string GetPriority()
+        {
+            return _process.PriorityClass.ToString();
+        }
+
+        public string GetStartCommand()
+        {
+            var startInfo = _process.GetType().GetField("startInfo", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic).GetValue(_process) as ProcessStartInfo;
+            
+            return startInfo?.FileName + " " + startInfo?.Arguments;
+        }
+
+        public bool HasFinished()
+        {
+            return _process.HasExited;
+        }
+
+        public void Refresh()
+        {
+            this._process.Refresh();
+        }
+    }
+}
--- wpf_process_manager/ProcessManager/ProcessManager.cs
+++ wpf_process_manager/ProcessManager/ProcessManager.cs
@@ -4,17 +4,64 @@ using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Threading;
+using wpf_process_manager.Models;
 
 namespace wpf_process_manager.ProcessManager
 {
     public class ProcessManager
     {
-        private IList<Process> _processes;
+        private List<ProcessModel> _processes;
+        private DispatcherTimer _refreshTimer;
         public ProcessManager()
         {
-            _processes = new List<Process>();
+            _processes = new List<ProcessModel>();
+            _refreshTimer = new DispatcherTimer();
+            _refreshTimer.Interval = TimeSpan.FromSeconds(3); // TODO
+            _refreshTimer.Tick += AutoRefresh;
         }
 
-        public 
+        public Process[] GetRunningProcesses()
+        {
+            return Process.GetProcesses();
+
+        }
+
+        private void OnRequestTickData()
+        {
+            // TODO: return values
+        }
+
+        private void AutoRefresh(object sender, EventArgs e)
+        {
+            Refresh();
+        }
+
+        public List<ProcessModel> Refresh()
+        {
+            var finishedProcesses = _processes.RemoveAll(x => x.HasFinished() == true);
+            Process[] processes = GetRunningProcesses();
+
+            // Update processes that already exist in the list
+            foreach (Process process in processes)
+            {
+                var proc = _processes.Find(x => x.GetPID() == process.Id);
+                if (proc == null)
+                {
+                    _processes.Add(new ProcessModel(process));
+                }
+                else
+                {
+                    proc.Refresh();
+                }
+            }
+
+            if (_refreshTimer.IsEnabled)
+            {
+                OnRequestTickData();
+            }
+
+            return this._processes;
+        }
     }
 }
