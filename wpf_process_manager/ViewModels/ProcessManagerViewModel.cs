beba48363f82f5fc89d9d78de7b46ba76c7ed48b
Wojciech Panfil <wojtek.panfil@hotmail.com>
2023-11-22 20:08:04 +0100

    Launch processes update on new Thread
    Avoid UI being stuck for some time
    
    Signed-off-by: Wojciech Panfil <wojtek.panfil@hotmail.com>

--- wpf_process_manager/ViewModels/ProcessManagerViewModel.cs
+++ wpf_process_manager/ViewModels/ProcessManagerViewModel.cs
@@ -1,12 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using System.ComponentModel;
 using System.Diagnostics;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Threading;
 using wpf_process_manager.Models;
 using wpf_process_manager.ProcessManager;
 
@@ -81,6 +76,12 @@ namespace wpf_process_manager.ViewModels
 
         /* Commands */
         private void Refresh()
+        {
+            var thread = new Thread(() => RefreshThreadWrapper());
+            thread.Start();
+        }
+
+        private void RefreshThreadWrapper()
         {
             Processes = new ObservableCollection<ProcessModel>(_processManager.Refresh());
         }
